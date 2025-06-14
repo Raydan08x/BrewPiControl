7bff81097cecf5d4c977b1485afcdfaed80c88a7
Raydan08x <cmadero08x@gmail.com>
2025-06-14 02:25:42 -0500

    Create index.d.ts

---
+++ frontend/webapp/src/contexts/index.d.ts
@@ -0,0 +1,7 @@
+// Definición de tipos para el módulo contexts
+declare module 'contexts' {
+  // Aquí puedes agregar tipos específicos según sea necesario
+  const contexts: any;
+  export default contexts;
+  export * from 'contexts';
+}
