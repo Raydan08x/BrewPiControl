5ddece86a332ab9554e0642cdfdbf30528d18818
Raydan08x <cmadero08x@gmail.com>
2025-06-14 02:26:17 -0500

    Update vite.config.ts

--- frontend/webapp/vite.config.ts
+++ frontend/webapp/vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  resolve: {
+    alias: {
+      api: resolve(__dirname, './src/api'),
+      components: resolve(__dirname, './src/components'),
+      contexts: resolve(__dirname, './src/contexts'),
+      hooks: resolve(__dirname, './src/hooks'),
+      store: resolve(__dirname, './src/store'),
+      views: resolve(__dirname, './src/views')
+    }
+  },
+  esbuild: {
+    // Esto ayuda a ignorar errores de tipos durante la compilación
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
   },
 });
