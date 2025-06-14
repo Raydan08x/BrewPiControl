2f37c82320b1b66940a72ce463fe21c19dd2b5be
Raydan08x <cmadero08x@gmail.com>
2025-06-14 02:25:59 -0500

    Create index.d.ts

---
+++ frontend/webapp/src/views/index.d.ts
@@ -0,0 +1,7 @@
+// Definición de tipos para el módulo views
+declare module 'views' {
+  // Aquí puedes agregar tipos específicos según sea necesario
+  const views: any;
+  export default views;
+  export * from 'views';
+}
