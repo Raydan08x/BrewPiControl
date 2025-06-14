ac9a1acd31fd370c61667355e28ab3d25c4fe0ad
Raydan08x <cmadero08x@gmail.com>
2025-06-14 02:25:50 -0500

    Create index.d.ts

---
+++ frontend/webapp/src/hooks/index.d.ts
@@ -0,0 +1,7 @@
+// Definición de tipos para el módulo hooks
+declare module 'hooks' {
+  // Aquí puedes agregar tipos específicos según sea necesario
+  const hooks: any;
+  export default hooks;
+  export * from 'hooks';
+}
