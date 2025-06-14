b2cd3d8cb4356c00fd789acb98175b2bfdaae599
Raydan08x <cmadero08x@gmail.com>
2025-06-13 20:22:05 -0500

    Update main.tsx

--- frontend/webapp/src/main.tsx
+++ frontend/webapp/src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { Toaster } from 'react-hot-toast';
+
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <>
-      <App />
-      <Toaster position="top-right" />
-    </>
+    <App />
   </StrictMode>
 );
