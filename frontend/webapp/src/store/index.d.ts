230d0ddc0d0fd3d4f92d081573189affd2bea658
Raydan08x <cmadero08x@gmail.com>
2025-06-14 02:25:56 -0500

    Create index.d.ts

---
+++ frontend/webapp/src/store/index.d.ts
@@ -0,0 +1,7 @@
+// Definición de tipos para el módulo store
+declare module 'store' {
+  // Aquí puedes agregar tipos específicos según sea necesario
+  const store: any;
+  export default store;
+  export * from 'store';
+}
